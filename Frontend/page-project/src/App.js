070a2a9b474339627fa59f01a5648d92d76f5990
dragong-sm <117700630+dragong-sm@users.noreply.github.com>
2022-11-24 12:04:22 +0900

    frontend templete

--- Frontend/page-project/src/App.js
+++ Frontend/page-project/src/App.js
@@ -1,8 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import ContentPage from "./pages/ContentPage";
 
 function App() {
-  return <div className="App"></div>;
+  // 다이어리 불러오기 : getDiarys()
+
+  return (
+    <div className="App">
+      <HomePage />
+      <ContentPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ Frontend/page-project/src/api/getDiarys.js
@@ -0,0 +1,7 @@
+// 다이어리불러오기
+
+import axios from "axios";
+
+export const getDiarys = () => {
+  //
+};
