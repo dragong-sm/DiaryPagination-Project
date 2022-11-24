580e445af7f7beabb101e43303a03b84b0bb296c
dragong-sm <117700630+dragong-sm@users.noreply.github.com>
2022-11-24 14:52:22 +0900

    base
    - route
    - axios

--- Frontend/page-project/src/api/getDiarys.js
+++ Frontend/page-project/src/api/getDiarys.js
@@ -2,6 +2,11 @@
 
 import axios from "axios";
 
-export const getDiarys = () => {
-  //
+export const getDiarys = async ({ currentPage, postPerPage }) => {
+  const response = await axios.get("", {
+    params: {
+      page: currentPage,
+      perPage: postPerPage,
+    },
+  });
 };
--- Frontend/page-project/src/components/Contents.js
+++ Frontend/page-project/src/components/Contents.js
@@ -2,11 +2,17 @@
 // 뒤로가기버튼(goBackBtn), 수정버튼(editBtn)
 
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
-const Contents = () => {
-  //
-  return <></>;
+const Contents = (contents) => {
+  console.log(contents);
+  return (
+    <div>
+      <h3>내용</h3>
+      {/* {contents} */}
+      <button>수정</button>
+    </div>
+  );
 };
 
 export default Contents;
