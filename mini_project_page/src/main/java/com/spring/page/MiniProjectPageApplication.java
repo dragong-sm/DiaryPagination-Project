994b0225d85c87cd1a0256ef7b668b2deb266d75
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-11-24 11:02:59 +0900

    ss

---
+++ mini_project_page/src/main/java/com/spring/page/MiniProjectPageApplication.java
@@ -0,0 +1,13 @@
+package com.spring.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectPageApplication.class, args);
+	}
+
+}
