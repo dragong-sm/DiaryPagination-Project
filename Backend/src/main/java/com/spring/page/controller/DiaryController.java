b6ab7e710f96aa6f614676aa80c464635dd66be7
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-11-25 12:13:35 +0900

    12시

--- Backend/src/main/java/com/spring/page/controller/DiaryController.java
+++ Backend/src/main/java/com/spring/page/controller/DiaryController.java
@@ -7,6 +7,7 @@ import java.util.stream.IntStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -23,6 +24,7 @@ import com.spring.page.dto.DiaryDTO;
 import com.spring.page.service.DiaryServiceImpl;
 import com.spring.page.service.FileServiceImpl;
 
+@CrossOrigin({"*"})
 @RestController
 public class DiaryController {	
 
